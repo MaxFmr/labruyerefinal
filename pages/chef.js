2b2df284347b518e150746f3f62f6632376b4106
Maxime Kerlidou <87577502+MaxFmr@users.noreply.github.com>
2022-10-24 11:12:30 +0200

    correction

--- pages/chef.js
+++ pages/chef.js
@@ -70,7 +70,7 @@ const Chef = () => {
         photoPath={'/assets/photos/chef/4.jpg'}
         title={'Sur mesure'}
         ImageSide='left'
-        text='Reservez une prestation sur mersure. Nous procédons à la création d’un menu qui correspond précisément à vos attentes, contrairement à d’autres maisons où la prestation est prédifinie par un menu existant.'
+        text='Reservez une prestation sur mersure. Nous procédons à la création d’un menu qui correspond précisément à vos attentes.'
         link={'/chef#form'}
         textLink='Réserver'
       />
